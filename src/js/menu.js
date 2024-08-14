d1a08601c867be3301a7be3cc006fabd95108b14
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-08-14 11:52:48 -0300

    criando a função para fechar o menu ao clicar em alguma opção

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,16 @@
 const $menu = document.querySelector('.menu');
 const $openMenuBtn = document.querySelector('.header__button');
 const $closeMenuBtn = document.querySelector('.menu__button');
+const $menuOption = document.querySelector('.menu__option');
 
 $openMenuBtn.addEventListener('click', () => {
     $menu.classList.toggle('menu__open');
 })
 
-console.log($closeMenuBtn);
-
 $closeMenuBtn.addEventListener('click', () => {
+    $menu.classList.toggle('menu__open');
+})
+
+$menuOption.addEventListener('click', () => {
     $menu.classList.toggle('menu__open');
 })
